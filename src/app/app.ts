aba6ab0000c139ea9d8dcf1631d5b19bbb06fed5
mdanassaifi <cscanas533@gmail.com>
2025-08-30 14:29:21 +0530

    bug fixed

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 
+
 @Component({
   selector: 'app-root',
   imports: [Header , RouterOutlet],
